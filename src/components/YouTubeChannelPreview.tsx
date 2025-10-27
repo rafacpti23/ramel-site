de190e6222a9714ea3be5aed253a8be4404c6743
Ramel Tecnologia <146174365+rafacpti23@users.noreply.github.com>
2025-10-26 23:54:32 -0300

    Update YouTubeChannelPreview.tsx

--- src/components/YouTubeChannelPreview.tsx
+++ src/components/YouTubeChannelPreview.tsx
@@ -34,7 +34,7 @@ const YouTubeChannelPreview = () => {
           <iframe
             width="100%"
             height="100%"
-            src={`https://www.youtube.com/watch?v=m0dOvJ62Gs4&list=PLDai3Q4XfkKoDnjSF683sPS57ogG5N6BF&index=2autoplay=0&mute=0`}
+            src={`https://www.youtube.com/watch?v=m0dOvJ62Gs4&list=PLDai3Q4XfkKoDnjSF683sPS57ogG5N6BF&index=2`}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
